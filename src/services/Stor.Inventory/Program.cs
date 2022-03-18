af7594f6f8e3b1b5db32cf7ab5f7847fb2f9b1e7
Iury F. A. Ferreira <iury.ferreira@modal.com.br>
2022-03-18 01:26:22 -0300

    :heavy_minus_sign: remove unused packages

--- src/services/Stor.Inventory/Program.cs
+++ src/services/Stor.Inventory/Program.cs
@@ -7,7 +7,6 @@ Container.BuildProvider();
 
 var config = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "akka.conf"));
 var akkaConfig = ConfigurationFactory.ParseString(config);
-//.WithFallback(ClusterMetrics.DefaultConfig());
 
 var system = ActorSystem.Create("stor", akkaConfig);
 system.ActorOf(InventoryActor.Props(), InventoryActor.ActorName);
